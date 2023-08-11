29e928b950c458186027ac3e7c34a7dd7a8124e5
Joaquim G. Pires <joaquim.pires@sptech.school>
2023-08-10 21:19:05 -0300

    starting guide

--- vextudio/app.vue
+++ vextudio/app.vue
@@ -1,26 +1,6 @@
 <template>
-	<div id="app">
-		<p class="text-[#010101]">Copos: {{ cupsOfWater }}</p>
-		<p class="text-[#010101]">Último copo: {{ lastCup }}</p>
-		<button class="bg-black text-white" @click="drinkCup">Beber um copo</button>
-	</div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-<script lang="ts">
-export default {
-	setup() {
-		const cupsOfWater: globalThis.Ref<number> = ref(0);
-		const lastCup: globalThis.Ref<Date> = ref(new Date());
-		
-		function drinkCup(): void {
-			cupsOfWater.value++
-			lastCup.value = new Date()
-		}
-
-		return {
-			cupsOfWater,
-			lastCup,
-			drinkCup
-		}
-	}
-}
-</script>
+<script lang="ts"></script>
---
+++ vextudio/pages/projects/[id].vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Product</p>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  setup() {
+    const cupsOfWater: globalThis.Ref<number> = ref(0);
+    const lastCup: globalThis.Ref<Date> = ref(new Date());
+
+    function drinkCup(): void {
+      cupsOfWater.value++;
+      lastCup.value = new Date();
+    }
+
+    return {
+      cupsOfWater,
+      lastCup,
+      drinkCup,
+    };
+  },
+};
+</script>
